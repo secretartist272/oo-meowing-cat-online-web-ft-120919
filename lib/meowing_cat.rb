7b9333a878e1f9840c3d22c7f7913d40b32e9d4f
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-19 05:28:43 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -2,18 +2,6 @@
 class Cat 
   attr_accessor :meow
   
-  def initialize(name)
-    @name = name
-  end
- 
-  def name
-    @name = "Maru"
-  end
-  
-  def name
-    name = "meow"
-  end
-  
   def meow
     puts "meow!"
   end
